ba7952bed186febf8365a23055a00f1c04a5f440
L0G1C06 <justgithub70@gmail.com>
2024-02-08 14:18:20 -0300

    project done

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -25,8 +25,10 @@ export class DetailsComponent {
     email: new FormControl('')
   });
   constructor() {
-      const housingLocationId = Number(this.route.snapshot.params['id'])
-      this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+      const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+      this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+        this.housingLocation = housingLocation;
+      }) 
   }
   submitApplication(){
     this.housingService.submitApplication(
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,7 +31,9 @@ export class HomeComponent {
   }
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 }
